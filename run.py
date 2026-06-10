ff01b2df43a8e417312b57dd4b8def13508a86c1
Geoffroy-Dhn <ciaociro2006@gmail.com>
2026-06-10 21:18:16 +0100

    Add files via upload
    Le projet final

---
+++ run.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+import sys
+import io
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+sys.stdin = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
+from backend.app import create_app
+
+app, socketio = create_app()
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
